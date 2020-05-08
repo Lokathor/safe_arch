3800a806d2cd67fd8dc20e466ef67205d2d7812e
Lokathor <zefria@gmail.com>
2020-05-08 00:51:41 -0600

    aes

--- src/lib.rs
+++ src/lib.rs
@@ -179,6 +179,8 @@ pub mod intel {
   submodule!(pub ssse3);
   #[cfg(target_feature = "sse4.1")]
   submodule!(pub sse4_1);
+  #[cfg(target_feature = "aes")]
+  submodule!(pub aes);
 }
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub use intel::*;
