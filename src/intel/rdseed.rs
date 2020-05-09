26e8576db2386c3041300b0f0b0fad33ff2ff4a8
Lokathor <zefria@gmail.com>
2020-05-09 11:14:20 -0600

    Update rdseed.rs

--- src/intel/rdseed.rs
+++ src/intel/rdseed.rs
@@ -6,38 +6,38 @@ use super::*;
 /// ```
 /// # use safe_arch::*;
 /// let mut val = 0_u16;
-/// let it_worked = rdrand_u16(&mut val);
+/// let it_worked = rdseed_u16(&mut val);
 /// ```
 #[must_use]
 #[inline(always)]
-#[cfg_attr(docs_rs, doc(cfg(target_feature = "rdrand")))]
+#[cfg_attr(docs_rs, doc(cfg(target_feature = "rdseed")))]
 pub fn rdseed_u16(out: &mut u16) -> i32 {
-  unsafe { _rdrand16_step(out) }
+  unsafe { _rdseed16_step(out) }
 }
 
 /// Try to obtain a random `u32` from the hardware RNG.
 /// ```
 /// # use safe_arch::*;
 /// let mut val = 0_u32;
-/// let it_worked = rdrand_u32(&mut val);
+/// let it_worked = rdseed_u32(&mut val);
 /// ```
 #[must_use]
 #[inline(always)]
-#[cfg_attr(docs_rs, doc(cfg(target_feature = "rdrand")))]
+#[cfg_attr(docs_rs, doc(cfg(target_feature = "rdseed")))]
 pub fn rdseed_u32(out: &mut u32) -> i32 {
-  unsafe { _rdrand32_step(out) }
+  unsafe { _rdseed32_step(out) }
 }
 
 /// Try to obtain a random `u64` from the hardware RNG.
 /// ```
 /// # use safe_arch::*;
 /// let mut val = 0_u64;
-/// let it_worked = rdrand_u64(&mut val);
+/// let it_worked = rdseed_u64(&mut val);
 /// ```
 #[must_use]
 #[inline(always)]
 #[cfg(target_arch = "x86_64")]
-#[cfg_attr(docs_rs, doc(cfg(target_feature = "rdrand")))]
+#[cfg_attr(docs_rs, doc(cfg(target_feature = "rdseed")))]
 pub fn rdseed_u64(out: &mut u64) -> i32 {
-  unsafe { _rdrand64_step(out) }
+  unsafe { _rdseed64_step(out) }
 }
