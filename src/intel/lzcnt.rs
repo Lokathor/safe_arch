b7e71842b07cdf6edf1c9d530998e50f49c7b081
Lokathor <zefria@gmail.com>
2020-05-10 16:12:44 -0600

    Update lzcnt.rs

--- src/intel/lzcnt.rs
+++ src/intel/lzcnt.rs
@@ -10,7 +10,7 @@ use super::*;
 /// ```
 #[must_use]
 #[inline(always)]
-#[cfg_attr(docs_rs, doc(cfg(target_feature = "sse")))]
+#[cfg_attr(docs_rs, doc(cfg(target_feature = "lzcnt")))]
 pub fn leading_zero_count_u32(a: u32) -> u32 {
   unsafe { _lzcnt_u32(a) }
 }
@@ -24,7 +24,7 @@ pub fn leading_zero_count_u32(a: u32) -> u32 {
 #[must_use]
 #[inline(always)]
 #[cfg(target_arch = "x86_64")]
-#[cfg_attr(docs_rs, doc(cfg(target_feature = "sse")))]
+#[cfg_attr(docs_rs, doc(cfg(target_feature = "lzcnt")))]
 pub fn leading_zero_count_u64(a: u64) -> u64 {
   unsafe { _lzcnt_u64(a) }
 }
