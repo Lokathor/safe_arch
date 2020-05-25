46b343f4a2d7a85eb1de217381fcc6f302e5b661
Soveu <marx.tomasz@gmail.com>
2020-05-25 12:31:34 +0200

    fma: extend docs

--- src/x86_x64/fma.rs
+++ src/x86_x64/fma.rs
@@ -11,6 +11,8 @@ use super::*;
 /// let d = mul_add_m256d(a, b, c).to_array();
 /// assert_eq!(d, [6.0, 13.0, 22.0, 33.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fmadd_pd`]
+/// * **Assembly:** `vfmadd132pd ymm, ymm, ymm`, `vfmadd213pd ymm, ymm, ymm`, `vfmadd231pd ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -27,6 +29,8 @@ pub fn mul_add_m256d(a: m256d, b: m256d, c: m256d) -> m256d {
 /// let d = mul_add_m256(a, b, c).to_array();
 /// assert_eq!(d, [6.0, 13.0, 22.0, 33.0, 6.0, 13.0, 22.0, 33.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fmadd_ps`]
+/// * **Assembly:** `vfmadd132ps ymm, ymm, ymm`, `vfmadd213ps ymm, ymm, ymm`, `vfmadd231ps ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -44,6 +48,8 @@ pub fn mul_add_m256(a: m256, b: m256, c: m256) -> m256 {
 /// let d = mul_addsub_m256d(a, b, c).to_array();
 /// assert_eq!(d, [4.0, 13.0, 20.0, 33.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fmaddsub_pd`]
+/// * **Assembly:** `vfmaddsub132pd ymm, ymm, ymm`, `vfmaddsub213pd ymm, ymm, ymm`, `vfmaddsub231pd ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -61,6 +67,8 @@ pub fn mul_addsub_m256d(a: m256d, b: m256d, c: m256d) -> m256d {
 /// let d = mul_addsub_m256(a, b, c).to_array();
 /// assert_eq!(d, [4.0, 13.0, 20.0, 33.0, 4.0, 13.0, 20.0, 33.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fmaddsub_ps`]
+/// * **Assembly:** `vfmaddsub132ps ymm, ymm, ymm`, `vfmaddsub213ps ymm, ymm, ymm`, `vfmaddsub231ps ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -78,6 +86,8 @@ pub fn mul_addsub_m256(a: m256, b: m256, c: m256) -> m256 {
 /// let d = mul_subadd_m256d(a, b, c).to_array();
 /// assert_eq!(d, [6.0, 11.0, 22.0, 31.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fmsubadd_pd`]
+/// * **Assembly:** `vfmsubadd132pd ymm, ymm, ymm`, `vfmsubadd213pd ymm, ymm, ymm`, `vfmsubadd231pd ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -95,6 +105,8 @@ pub fn mul_subadd_m256d(a: m256d, b: m256d, c: m256d) -> m256d {
 /// let d = mul_subadd_m256(a, b, c).to_array();
 /// assert_eq!(d, [6.0, 11.0, 22.0, 31.0, 6.0, 11.0, 22.0, 31.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fmsubadd_ps`]
+/// * **Assembly:** `vfmsubadd132ps ymm, ymm, ymm`, `vfmsubadd213ps ymm, ymm, ymm`, `vfmsubadd231ps ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -111,6 +123,8 @@ pub fn mul_subadd_m256(a: m256, b: m256, c: m256) -> m256 {
 /// let d = mul_sub_m256d(a, b, c).to_array();
 /// assert_eq!(d, [4.0, 11.0, 20.0, 31.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fmsub_pd`]
+/// * **Assembly:** `vfmsub132pd ymm, ymm, ymm`, `vfmsub213pd ymm, ymm, ymm`, `vfmsub231pd ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -127,6 +141,8 @@ pub fn mul_sub_m256d(a: m256d, b: m256d, c: m256d) -> m256d {
 /// let d = mul_sub_m256(a, b, c).to_array();
 /// assert_eq!(d, [4.0, 11.0, 20.0, 31.0, 4.0, 11.0, 20.0, 31.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fmsub_ps`]
+/// * **Assembly:** `vfmsub132ps ymm, ymm, ymm`, `vfmsub213ps ymm, ymm, ymm`, `vfmsub231ps ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -143,6 +159,8 @@ pub fn mul_sub_m256(a: m256, b: m256, c: m256) -> m256 {
 /// let d = mul_neg_add_m256d(a, b, c).to_array();
 /// assert_eq!(d, [-4.0, -11.0, -20.0, -31.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fnmadd_pd`]
+/// * **Assembly:** `vfnmadd132pd ymm, ymm, ymm`, `vfnmadd213pd ymm, ymm, ymm`, `vfnmadd231pd ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -159,6 +177,8 @@ pub fn mul_neg_add_m256d(a: m256d, b: m256d, c: m256d) -> m256d {
 /// let d = mul_neg_add_m256(a, b, c).to_array();
 /// assert_eq!(d, [-4.0, -11.0, -20.0, -31.0, -4.0, -11.0, -20.0, -31.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fnmadd_ps`]
+/// * **Assembly:** `vfnmadd132ps ymm, ymm, ymm`, `vfnmadd213ps ymm, ymm, ymm`, `vfnmadd231ps ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -175,6 +195,8 @@ pub fn mul_neg_add_m256(a: m256, b: m256, c: m256) -> m256 {
 /// let d = mul_neg_sub_m256d(a, b, c).to_array();
 /// assert_eq!(d, [-6.0, -13.0, -22.0, -33.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fnmsub_pd`]
+/// * **Assembly:** `vfnmsub132pd ymm, ymm, ymm`, `vfnmsub213pd ymm, ymm, ymm`, `vfnmsub231pd ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -191,6 +213,8 @@ pub fn mul_neg_sub_m256d(a: m256d, b: m256d, c: m256d) -> m256d {
 /// let d = mul_neg_sub_m256(a, b, c).to_array();
 /// assert_eq!(d, [-6.0, -13.0, -22.0, -33.0, -6.0, -13.0, -22.0, -33.0]);
 /// ```
+/// * **Intrinsic:** [`_mm256_fnmsub_ps`]
+/// * **Assembly:** `vfnmsub132ps ymm, ymm, ymm`, `vfnmsub213ps ymm, ymm, ymm`, `vfnmsub231ps ymm, ymm, ymm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -207,6 +231,8 @@ pub fn mul_neg_sub_m256(a: m256, b: m256, c: m256) -> m256 {
 /// let d = mul_add_m128d(a, b, c).to_array();
 /// assert_eq!(d, [9.0, 16.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmadd_pd`]
+/// * **Assembly:** `vfmadd132pd xmm, xmm, xmm`, `vfmadd213pd xmm, xmm, xmm`, `vfmadd231pd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -223,6 +249,8 @@ pub fn mul_add_m128d(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_add_m128(a, b, c).to_array();
 /// assert_eq!(d, [9.0, 16.0, 25.0, 36.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmadd_ps`]
+/// * **Assembly:** `vfmadd132ps xmm, xmm, xmm`, `vfmadd213ps xmm, xmm, xmm`, `vfmadd231ps xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -239,6 +267,8 @@ pub fn mul_add_m128(a: m128, b: m128, c: m128) -> m128 {
 /// let d = mul_add_m128d_s(a, b, c).to_array();
 /// assert_eq!(d, [9.0, 3.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmadd_sd`]
+/// * **Assembly:** `vfmadd132sd xmm, xmm, xmm`, `vfmadd213sd xmm, xmm, xmm`, `vfmadd231sd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -255,6 +285,8 @@ pub fn mul_add_m128d_s(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_add_m128_s(a, b, c).to_array();
 /// assert_eq!(d, [9.0, 3.0, 4.0, 5.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmadd_ss`]
+/// * **Assembly:** `vfmadd132ss xmm, xmm, xmm`, `vfmadd213ss xmm, xmm, xmm`, `vfmadd231ss xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -272,6 +304,8 @@ pub fn mul_add_m128_s(a: m128, b: m128, c: m128) -> m128 {
 /// let d = mul_addsub_m128d(a, b, c).to_array();
 /// assert_eq!(d, [7.0, 16.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmaddsub_pd`]
+/// * **Assembly:** `vfmaddsub132pd xmm, xmm, xmm`, `vfmaddsub213pd xmm, xmm, xmm`, `vfmaddsub231pd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -289,6 +323,8 @@ pub fn mul_addsub_m128d(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_addsub_m128(a, b, c).to_array();
 /// assert_eq!(d, [7.0, 16.0, 23.0, 36.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmaddsub_ps`]
+/// * **Assembly:** `vfmaddsub132ps xmm, xmm, xmm`, `vfmaddsub213ps xmm, xmm, xmm`, `vfmaddsub231ps xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -305,6 +341,8 @@ pub fn mul_addsub_m128(a: m128, b: m128, c: m128) -> m128 {
 /// let d = mul_sub_m128d(a, b, c).to_array();
 /// assert_eq!(d, [7.0, 14.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmsub_pd`]
+/// * **Assembly:** `vfmsub132pd xmm, xmm, xmm`, `vfmsub213pd xmm, xmm, xmm`, `vfmsub231pd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -321,6 +359,8 @@ pub fn mul_sub_m128d(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_sub_m128(a, b, c).to_array();
 /// assert_eq!(d, [7.0, 14.0, 23.0, 34.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmsub_ps`]
+/// * **Assembly:** `vfmsub132ps xmm, xmm, xmm`, `vfmsub213ps xmm, xmm, xmm`, `vfmsub231ps xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -337,6 +377,8 @@ pub fn mul_sub_m128(a: m128, b: m128, c: m128) -> m128 {
 /// let d = mul_sub_m128d_s(a, b, c).to_array();
 /// assert_eq!(d, [7.0, 3.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmsub_sd`]
+/// * **Assembly:** `vfmsub132sd xmm, xmm, xmm`, `vfmsub213sd xmm, xmm, xmm`, `vfmsub231sd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -353,6 +395,8 @@ pub fn mul_sub_m128d_s(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_sub_m128_s(a, b, c).to_array();
 /// assert_eq!(d, [7.0, 3.0, 4.0, 5.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmsub_ss`]
+/// * **Assembly:** `vfmsub132ss xmm, xmm, xmm`, `vfmsub213ss xmm, xmm, xmm`, `vfmsub231ss xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -370,6 +414,8 @@ pub fn mul_sub_m128_s(a: m128, b: m128, c: m128) -> m128 {
 /// let d = mul_subadd_m128d(a, b, c).to_array();
 /// assert_eq!(d, [9.0, 14.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmsubadd_pd`]
+/// * **Assembly:** `vfmsubadd132pd xmm, xmm, xmm`, `vfmsubadd213pd xmm, xmm, xmm`, `vfmsubadd231pd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -387,6 +433,8 @@ pub fn mul_subadd_m128d(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_subadd_m128(a, b, c).to_array();
 /// assert_eq!(d, [9.0, 14.0, 25.0, 34.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fmsubadd_ps`]
+/// * **Assembly:** `vfmsubadd132ps xmm, xmm, xmm`, `vfmsubadd213ps xmm, xmm, xmm`, `vfmsubadd231ps xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -403,6 +451,8 @@ pub fn mul_subadd_m128(a: m128, b: m128, c: m128) -> m128 {
 /// let d = mul_neg_add_m128d(a, b, c).to_array();
 /// assert_eq!(d, [-7.0, -14.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fnmadd_pd`]
+/// * **Assembly:** `vfnmadd132pd xmm, xmm, xmm`, `vfnmadd213pd xmm, xmm, xmm`, `vfnmadd231pd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -419,6 +469,8 @@ pub fn mul_neg_add_m128d(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_neg_add_m128(a, b, c).to_array();
 /// assert_eq!(d, [-7.0, -14.0, -23.0, -34.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fnmadd_ps`]
+/// * **Assembly:** `vfnmadd132ps xmm, xmm, xmm`, `vfnmadd213ps xmm, xmm, xmm`, `vfnmadd231ps xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -435,6 +487,8 @@ pub fn mul_neg_add_m128(a: m128, b: m128, c: m128) -> m128 {
 /// let d = mul_neg_add_m128d_s(a, b, c).to_array();
 /// assert_eq!(d, [-7.0, 3.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fnmadd_sd`]
+/// * **Assembly:** `vfnmadd132sd xmm, xmm, xmm`, `vfnmadd213sd xmm, xmm, xmm`, `vfnmadd231sd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -451,6 +505,8 @@ pub fn mul_neg_add_m128d_s(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_neg_add_m128_s(a, b, c).to_array();
 /// assert_eq!(d, [-7.0, 3.0, 4.0, 5.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fnmadd_ss`]
+/// * **Assembly:** `vfnmadd132ss xmm, xmm, xmm`, `vfnmadd213ss xmm, xmm, xmm`, `vfnmadd231ss xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -467,6 +523,8 @@ pub fn mul_neg_add_m128_s(a: m128, b: m128, c: m128) -> m128 {
 /// let d = mul_neg_sub_m128d(a, b, c).to_array();
 /// assert_eq!(d, [-9.0, -16.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fnmsub_pd`]
+/// * **Assembly:** `vfnmsub132pd xmm, xmm, xmm`, `vfnmsub213pd xmm, xmm, xmm`, `vfnmsub231pd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -483,6 +541,8 @@ pub fn mul_neg_sub_m128d(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_neg_sub_m128(a, b, c).to_array();
 /// assert_eq!(d, [-9.0, -16.0, -25.0, -36.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fnmsub_ps`]
+/// * **Assembly:** `vfnmsub132ps xmm, xmm, xmm`, `vfnmsub213ps xmm, xmm, xmm`, `vfnmsub231ps xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -499,6 +559,8 @@ pub fn mul_neg_sub_m128(a: m128, b: m128, c: m128) -> m128 {
 /// let d = mul_neg_sub_m128d_s(a, b, c).to_array();
 /// assert_eq!(d, [-9.0, 3.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fnmsub_sd`]
+/// * **Assembly:** `vfnmsub132sd xmm, xmm, xmm`, `vfnmsub213sd xmm, xmm, xmm`, `vfnmsub231sd xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
@@ -515,6 +577,8 @@ pub fn mul_neg_sub_m128d_s(a: m128d, b: m128d, c: m128d) -> m128d {
 /// let d = mul_neg_sub_m128_s(a, b, c).to_array();
 /// assert_eq!(d, [-9.0, 3.0, 4.0, 5.0]);
 /// ```
+/// * **Intrinsic:** [`_mm_fnmsub_ss`]
+/// * **Assembly:** `vfnmsub132ss xmm, xmm, xmm`, `vfnmsub213ss xmm, xmm, xmm`, `vfnmsub231ss xmm, xmm, xmm`
 #[must_use]
 #[inline(always)]
 #[cfg_attr(docs_rs, doc(cfg(target_feature = "fma")))]
