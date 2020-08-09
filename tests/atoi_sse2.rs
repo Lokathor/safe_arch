5bd913c999ea5854de87c6ddbcfd3b928d06f55d
Lokathor <zefria@gmail.com>
2020-08-09 01:59:09 -0600

    restrict test to sse2 and up

---
+++ tests/atoi_sse2.rs
@@ -0,0 +1,28 @@
+use safe_arch::*;
+
+#[test]
+#[cfg(target_feature = "sse2")]
+fn atoi_test() {
+  fn atoi(x: [u8; 16]) -> u64 {
+    let ascii_zero = set_splat_i8_m128i(b'0' as i8);
+    let x: m128i = x.into();
+    let x = sub_i8_m128i(x, ascii_zero);
+
+    let tens = set_splat_i16_m128i(1 << 8 | 10);
+    let x = mul_u8i8_add_horizontal_saturating_m128i(x, tens); /* eeee macarena! */
+
+    let tens = set_splat_i32_m128i(1 << 16 | 100);
+    let x = mul_i16_horizontal_add_m128i(x, tens);
+
+    let tens = set_i16_m128i(0, 0, 0, 0, 1, 10000, 1, 10000);
+    let x = pack_i32_to_u16_m128i(x, x);
+    let x = mul_i16_horizontal_add_m128i(x, tens);
+
+    let x: [u32; 4] = x.into();
+    return x[1] as u64 + x[0] as u64 * 100000000;
+  }
+
+  assert_eq!(atoi(*b"1234567812345678"), 1234567812345678);
+  assert_eq!(atoi(*b"0000000000000000"), 0000000000000000);
+  assert_eq!(atoi(*b"1982379879823749"), 1982379879823749);
+}
