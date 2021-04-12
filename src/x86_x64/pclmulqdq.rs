30beeb7f1b84fd3ffcf6232cfb43246239b9f434
Lokathor <zefria@gmail.com>
2021-04-12 02:27:52 -0600

    docs.

--- src/x86_x64/pclmulqdq.rs
+++ src/x86_x64/pclmulqdq.rs
@@ -9,6 +9,9 @@ use super::*;
 /// * Bit 4: the `i64` index from `b` to multiply.
 ///
 /// The output is always in the low `i64` lane, with the high lane as 0.
+///
+/// * **Intrinsic:** [`_mm_clmulepi64_si128`]
+/// * **Assembly:** `pclmulqdq xmm, xmm, imm8`
 pub fn mul_i64_carryless_m128i<const IMM: i32>(a: m128i, b: m128i) -> m128i {
   m128i(unsafe { _mm_clmulepi64_si128(a.0, b.0, IMM) })
 }
