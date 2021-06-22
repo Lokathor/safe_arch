68abef090f871c10e42ef20069ac9b39554fb683
Lokathor <zefria@gmail.com>
2021-06-22 17:49:17 -0600

    move atoi_test to the correct file.

--- tests/integration/ssse3_tests.rs
+++ tests/integration/ssse3_tests.rs
@@ -1,6 +1,33 @@
 use super::*;
 
 #[test]
+fn atoi_test() {
+  fn atoi(x: [u8; 16]) -> u64 {
+    let ascii_zero = set_splat_i8_m128i(b'0' as i8);
+    let x: m128i = x.into();
+    let x = sub_i8_m128i(x, ascii_zero);
+
+    let tens = set_splat_i16_m128i(1 << 8 | 10);
+    let x = mul_u8i8_add_horizontal_saturating_m128i(x, tens); /* eeee macarena! */
+
+    let tens = set_splat_i32_m128i(1 << 16 | 100);
+    let x = mul_i16_horizontal_add_m128i(x, tens);
+
+    let tens = set_i16_m128i(0, 0, 0, 0, 1, 10000, 1, 10000);
+    let x = pack_i32_to_u16_m128i(x, x);
+    let x = mul_i16_horizontal_add_m128i(x, tens);
+
+    let x: [u32; 4] = x.into();
+    return x[1] as u64 + x[0] as u64 * 100000000;
+  }
+
+  assert_eq!(atoi(*b"1234567812345678"), 1234567812345678);
+  assert_eq!(atoi(*b"0000000000000000"), 0000000000000000);
+  assert_eq!(atoi(*b"1982379879823749"), 1982379879823749);
+}
+
+
+#[test]
 fn test_abs_i8_m128i() {
   let a = m128i::from([0_i8, -1, 2, -3, 4, -5, 6, -7, -8, 9, -10, 11, -12, 13, -14, -128]);
   let c: [i8; 16] = abs_i8_m128i(a).into();
@@ -15,7 +42,7 @@ fn test_abs_i16_m128i() {
 }
 
 #[test]
-fn test_() {
+fn test_abs_i32_m128i() {
   let a = m128i::from([0, -1, 2, i32::MIN]);
   let c: [i32; 4] = abs_i32_m128i(a).into();
   assert_eq!(c, [0, 1, 2, i32::MIN]);
