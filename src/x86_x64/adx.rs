ffe7f6caa9eff3a24d44d64f97461736aa85fa52
Lokathor <zefria@gmail.com>
2024-12-20 12:20:49 -0700

    docs formatting

--- src/x86_x64/adx.rs
+++ src/x86_x64/adx.rs
@@ -8,8 +8,10 @@ use super::*;
 ///
 /// * **Intrinsic:** [`_addcarryx_u32`]
 /// * **Assembly:**
-/// `adcx r32, r32`
-/// `adox r32, r32`
+/// ```asm
+/// adcx r32, r32
+/// adox r32, r32
+/// ```
 #[inline(always)]
 #[cfg_attr(docsrs, doc(cfg(target_feature = "adx")))]
 pub fn add_carry_u32(c_in: u8, a: u32, b: u32, out: &mut u32) -> u8 {
@@ -22,12 +24,13 @@ pub fn add_carry_u32(c_in: u8, a: u32, b: u32, out: &mut u32) -> u8 {
 ///
 /// * **Intrinsic:** [`_addcarryx_u64`]
 /// * **Assembly:**
-/// `adcx r64, r64`
-/// `adox r64, r64`
+/// ```asm
+/// adcx r64, r64
+/// adox r64, r64
+/// ```
 #[inline(always)]
 #[cfg_attr(docsrs, doc(cfg(target_feature = "adx")))]
 #[cfg(target_arch = "x86_64")]
 pub fn add_carry_u64(c_in: u8, a: u64, b: u64, out: &mut u64) -> u8 {
   unsafe { _addcarryx_u64(c_in, a, b, out) }
 }
-
